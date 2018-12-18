51d11ea8f8450ca9d19d78f75db96a474868d7fa
martinONS <martin.wood.ons@gmail.com>
2018-12-18 16:25:50 +0000

    Have working process for getting data from the PDF's, need to add in retrieval of reporting units and years as part of master function somehow.

--- xbrl_image_parser.py
+++ xbrl_image_parser.py
@@ -6,9 +6,12 @@ martin.wood@ons.gov.uk
 
 XBRL image parser
 
-Contains functions to take the data created by passing a company's
+Contains functions to take the data created by passing a company's PDF
 balance sheet through Google's Tesseract OCR software (v4), and extract
 useful financial variables from it.
+
+These processes are VERY reliant on the format of the output of the
+TesseractOCR software.
 
 Will eventually contain system calls for use on Linux to call the OCR
 process.
@@ -32,39 +35,15 @@ def make_measurements(data):
 	on the page.
 	"""
 	
-	# Creating some new useful variables
 	data['centre_x'] = data['left'] + ( data['width'] / 2. )
 	data['centre_y'] = data['top'] + ( data['height'] / 2. )
 	data['right'] = data['left'] + data['width']
 	data['bottom'] = data['top'] + data['height']
 	data['area'] = data['height'] * data['width']
 	
-	# Shifted values for calcuating word spacing
-	# This can be useful for identifying sparsely
-	data['space_from_left'] = np.where((data['word_num'] == 0) | (data['word_num'] == 1),
-									   np.nan,
-									   data['left'] - data['right'].shift())
+	return( data )
 	
-	# Data already ordered by page, block, paragraph, line and word number.
-	# Want to create unique line identifier.
-	line_lookup = data[['csv_num', 'page_num', 'block_num', 'par_num', 'line_num']].\
-		groupby(['csv_num', 'page_num', 'block_num', 'par_num', 'line_num']).\
-		size().\
-		reset_index().\
-		rename(columns={0:'line_word_count'})
 	
-	# Stick the line index back on to the data
-	line_lookup['line_index'] = line_lookup.index.values
-	line_data = data.merge(line_lookup, on=['csv_num', 'page_num', 'block_num', 'par_num', 'line_num'], how="left")
-	
-	# Calculate some whole_document spacing statistics
-	line_data['doc_spacing_mean'] = line_data['space_from_left'].sum() / line_data['space_from_left'].count()
-	line_data['doc_spacing_median'] = line_data['space_from_left'].median()
-	line_data['doc_spacing_sd'] = np.sqrt( np.sum( (line_data['space_from_left'] - line_data['doc_spacing_mean']) ** 2. ) / (line_data['space_from_left'].count() - 1.) )
-
-	return( line_data )
-
-
 def convert_to_numeric(series):
 	"""
 	Converts a pandas series object (of strings) to numeric if possible.
@@ -72,25 +51,7 @@ def convert_to_numeric(series):
 	"""
 	q_func = lambda x: str(x).replace(",", "").strip("(").strip(")")
 	
-	numeric_series = pd.to_numeric(series.apply(q_func), errors="coerce")             # If errors, force process to continue, invalid element returned as numpy.nan
-	
-	return(numeric_series)
-
-
-def find_pages(dat, keystring):
-	"""
-	Filter to pages (using csv_num) that have a mention of a specific
-	phrase or word on any line.  This is meant to reduce the risk of the
-	software consulting the wrong table.
-	"""
-	
-	dat_agg = agg_level(dat, 4)
-	
-	dat_agg = dat_agg[dat_agg['text'].apply(lambda x: keystring in x.lower())]
-	
-	csv_numbers = dat_agg['csv_num'].unique()
-	
-	return(dat[dat['csv_num'].apply(lambda x: x in csv_numbers)])
+	return( pd.to_numeric(series.apply(q_func), errors="coerce") ) # If errors, force process to continue, invalid element returned as numpy.nan
 
 
 def determine_units_count(subset):
@@ -161,7 +122,7 @@ def aggregate_sentences_over_lines(dat):
 	line_text = dat_group['text'].apply(lambda x: " ".join([str(e) for e in list(x)]).strip("nan "))
 	line_text = line_text.reset_index()
 	
-	# Create line bounding boxes for page
+	# Create line bounding boxes for line groups
 	line_text['top'] = dat_group['top'].agg('min').reset_index()['top']
 	line_text['bottom'] = dat_group['bottom'].agg('max').reset_index()['bottom']
 	line_text['left'] = dat_group['left'].agg('min').reset_index()['left']
@@ -201,245 +162,118 @@ def aggregate_sentences_over_lines(dat):
 	return(results.drop("continued_line", axis=1))
 
 
-########################################################################
-
-###  Everything beyond here's legacy code from the old approach ###
-
-########################################################################
-
-def agg_level(dat, level=4):
+def find_balance_sheet_pages(data):
 	"""
-	Text data read in by Tesseract OCR is grouped by level (highest
-	number is most granular and represents words, lowest number is least
-	granular and represents blocks of content).
-	
-	This function takes the data and aggregates, or rather concatenates,
-	text to a specified level.
-	
-	Facilitates search for more complex strings that may be split across
-	lines, paragraphs or blocks and so on.
-	
-	Default level indicates lines
+	Through holistic steps, identify pages likely to contain the balance
+	sheet.  This includes finding sentences starting
+	[abbreviated]*balancesheet, and excluding pages containing
+	'notestothefinancialstatements' and 'statementof'.
 	"""
 	
-	dat = dat[dat['level']>=level][['csv_num', 'block_num', 'par_num', 'line_num', 'text']]
-	
-	# Not certain the "nan" value stripper below will work as expected
-	mat = dat.groupby(["csv_num", "block_num", "par_num", "line_num"])['text'].\
-			   apply(lambda x: " ".join([str(e) for e in list(x)]).strip("nan ")).\
-			   reset_index()
-	
-	return(mat)
+	# Create a table with aggregated sentences over lines
+	agg_text = aggregate_sentences_over_lines(data)
 
+	# Get a list of pages likely to be balance sheets
+	BS_page_list = pd.unique(agg_text[agg_text['text'].apply(lambda x: np.where( re.search( "^[abbreviated]*balancesheet", x ), True, False))]['csv_num'])
 
-def agg_two_lines(dat):
-	"""
-	For each table entry, create a field that has the aggregated text of
-	all text from the line the entry is on, plus all text from the next
-	line the entry is on.  This is in order to create one field within
-	which to search for variable names that, if long, may be split over
-	two lines.
+	# Filter out any page with the words "notes to the financial statements"
+	notes_page_list = pd.unique(agg_text[agg_text['text'].apply(lambda x: "notestothefinancialstatements" in x)]['csv_num'])
 	
-	This function is assuming the data is ordered, as it is if you take 
-	it straight from TesseractOCR.
-	"""
-
-	# Create aggregated table of line text + indexing columns only
-	dat_agg = agg_level(dat)
+	# Filter out any page with the words "Statement of changes in equity"
+	statement_page_list = pd.unique(agg_text[agg_text['text'].apply(lambda x: "statementof" in x)]['csv_num'])
 	
-	# Append aggregated text column (each line + its next line)
-	dat_agg['text_agg'] = dat_agg['text'] + " " + dat_agg['text'].shift(-1)
-	
-	# drop old text column
-	dat_agg = dat_agg.drop('text', axis=1)
-	
-	# Merge 'text_agg' column onto original dataset, one to many
-	dat_merged = dat.merge(dat_agg,
-                on=["csv_num", "block_num", "par_num", "line_num"],
-                how="left")
-	
-	return(dat_merged)
-	#return(dat_merged[(dat_merged['word_num'] == 0) &
-	#				  (dat_merged['line_num'] > 0)])
-
-
-def extract_stat(dat, keyword):
-	"""
-	Implements a process for finding key numbers based upon position on
-	page - finds first the element name of interest ('keyword'), all
-	occurences.  Gets any numbers horizontally aligned with the keyword
-	and then returns the right-most two.
-	"""
-	
-	# Find examples of the keyword of interest
-	dat['keyword_found'] = dat['text']==keyword
-	
-	elements = pd.DataFrame()
-	
-	# For keyword, hunt down all numerical values roughly horizontally
-	# aligned with it
-	for index, row in dat[dat['keyword_found']==True].iterrows():
-		
-		selection = dat[(dat['top'] < row['bottom']) &                  # Subset to elements roughly in line with keyword
-						(dat['bottom'] > row['top']) &                  # Remembering w. vertical position 0 is at top
-						(dat['csv_num'] == row['csv_num']) &            # Make sure we're (literally) on the same page
-						(dat['left'] > row['left'])]                    # Make sure the numbers are to the right of the row label
-						#(pd.isna(dat['numerical'])==False)]
-						   
-		# Assign an index to the search results matching the line index 
-		# of the original keyword.  Take the two right-most numbers.
-		selection['keyword_index'] = index
-		selection = selection.sort_values("left", ascending=False).\
-							  iloc[0:2,].\
-							  sort_values("left", ascending=True)
-		
-		print(selection['text'])
-		elements = elements.append(selection)
-	
-	elements = elements.iloc[0:2,]
-	
-	# Take only the first positive identified
-	return(tuple(elements['text']),
-		   tuple(elements['numerical']),
-		   tuple(elements['conf']))
-
-# Find a statistic in the discovered tables...UPGRADED!
-def extract_stat_linesearch(dat, keyphrase):
-	"""
-	Implements a process for finding key numbers based upon position on
-	page - finds first the element name of interest ('keyphrase'), all
-	occurences even if spread over two lines.  Gets any numbers
-	horizontally aligned with the keyphrase and then returns the 
-	right-most two.
-	"""
-	
-	dat_agg = agg_two_lines(dat)
-	
-	# Find examples of the keyword of interest
-	dat_agg = dat_agg[dat_agg['text_agg'].apply(lambda x: keyphrase in str(x))]
-	
-	# Filter to those which begin with the same first four characters
-	dat_agg = dat_agg[dat_agg['text_agg'].apply(lambda x: x.startswith(keyphrase[:4]))]
-	
-	# Take first result, hunt down all numbers aligned with it
-	row = dat_agg.iloc[0]
-	
-	selection = dat[(dat['top'] < row['bottom']) &                              # Subset to elements roughly in line with keyword
-					(dat['bottom'] > row['top']) &                              # Remembering w. vertical position 0 is at top
-					(dat['csv_num'] == row['csv_num']) &                        # Make sure we're (literally) on the same page
-					(dat['left'] > row['left']) &                               # Make sure the numbers are to the right of the row label
-					(pd.isna(dat['numerical'])==False)]			   
-
-	# Get the two right-most results
-	selection = selection.sort_values("left", ascending=False).\
-						  iloc[0:2,].\
-						  sort_values("left", ascending=True)
-	
-	# Record some metadata about the match found					  
-	selection['text_matched'] = row['text_agg']
-	selection['text_original'] = row['text']
-	
-	return(tuple(selection['numerical']),
-		   tuple(selection['conf']))
-
-
-def extract_parallel_numbers(dat, row):
-	"""
-	Finds and returns all numerical entries that are parallel on a page
-	to the text in 'row'.  Row should be a pandas Series object comprised
-	of a single extracted row from the DF created from the document.
-	"""
-	
-	selection = dat[(dat['top'] < int(row['bottom'])) &                              # Subset to elements roughly in line with keyword
-					(dat['bottom'] > int(row['top'])) &                              # Remembering w. vertical position 0 is at top
-					(dat['csv_num'] == int(row['csv_num'])) &                        # Make sure we're (literally) on the same page
-					(dat['left'] > int(row['left'])) &                               # Make sure the numbers are to the right of the row label
-					(pd.isna(dat['numerical'])==False)]	
-	
-	return(selection)
-
-def find_element_location(dat, keyphrase):
-	"""
-	Finds a row corresponding to the first word of a phrase of interest.
-	"""
-	
-	# Create an aggregated text field of the text from the whole line
-	# and the next line down
-	dat_agg = agg_two_lines(dat)
-	
-	# Find examples of the keyword of interest
-	dat_agg = dat_agg[dat_agg['text_agg'].apply(lambda x: keyphrase in str(x))]
-	
-	# Filter to those which begin with the same first four characters
-	dat_agg = dat_agg[dat_agg['text_agg'].apply(lambda x: x.startswith(keyphrase[:4]))]
-	
-	# Return one representative row, corresponding to the first word in
-	# the line/phrase.  Allows for multiple matches.
-	return(dat_agg[dat_agg['word_num'] == 1])	
+	return( [x for x in BS_page_list if x not in list(notes_page_list) + list(statement_page_list)] )
 	
 
-def extract_stat_linesearch2(dat, keyphrase):
+# Lifted this almost directly from David Kane's work
+def detect_lines(page_df, x_tolerance=0):
+    """
+    Detect lines in the csv of a page, returned by Tesseract
+    """
+    words_df = page_df[page_df['word_num'] > 0]
+    page_stats = page_df.iloc[0, :]
+    
+    row_ranges = []
+    this_range = []
+    
+    # Iterate through every vertical pixel position, top (0) to bottom (height)
+    for i in range(page_stats['height']):
+        result = (( words_df['bottom'] >= i ) & ( words_df['top'] <= i )).sum() > 0
+        
+        # Append vertical pixels aligned with words to this_range
+        if result:
+            this_range.append(i)
+        
+        # If we've passed out of an "occupied" range, append the resulting range to a list to store
+        else:
+            if this_range:
+                row_ranges.append(this_range)
+            this_range = []
+        
+    # Create bounding boxes for convenience
+    return[{"left":0, "right":page_stats['width'], "top":min(r), "bottom":max(r)} for r in row_ranges]
+    
+
+def extract_lines(page_df, lines):
+    
+    # Look, dark magic!
+    finance_regex = r'(.*)\s+(\(?\-?[\,0-9]+\)?)\s+(\(?\-?[\,0-9]+\)?)$'
+    
+    words_df = page_df[page_df['word_num'] > 0]
+    
+    raw_lines = []
+    results = pd.DataFrame()
+    for line in lines:
+        
+        # Retrieve all text in line
+        inline = (words_df['bottom'] <= line['bottom']) & (words_df['top'] >= line['top'])
+        line_text = " ".join([str(x) for x in words_df[inline]['text']] )
+        raw_lines.append(line_text)
+        
+        # Perform an incredibly complex regex search to extract right-most two numbers and the label
+        result = re.match(finance_regex, line_text)
+        
+        if result:
+            
+            # Check if label is a continuation, if so, append text from last line
+            if re.match(r'^[a-z]',re.sub("[0-9]", "", result.groups()[0]).strip()[0]):
+                label = raw_lines[-2] + " " + re.sub("[0-9]", "", result.groups()[0]).strip()
+            else:
+                label = re.sub("[0-9]", "", result.groups()[0]).strip()
+            
+            results = results.append({"label":label,
+                                      "CurrYr":result.groups()[1],
+                                      "LastYr":result.groups()[2],
+                                      "source":line_text},
+                                     ignore_index=True)
+    return(results)
+
+
+def process_OCR_csv(data):
 	"""
-	Implements a process for finding key numbers based upon position on
-	page - finds first the element name of interest ('keyphrase'), all
-	occurences even if spread over two lines.  Gets any numbers
-	horizontally aligned with the keyphrase and then returns the 
-	right-most two.
+	Call all the functions, get all the data...
 	"""
 	
-	dat_agg = agg_two_lines(dat)
+	# Do some geometry (eg; calculate specific coords of "bottom")
+	data = make_measurements(data)
 	
-	# Find examples of the keyword of interest
-	dat_agg = dat_agg[dat_agg['text_agg'].apply(lambda x: keyphrase in str(x))]
+	# Create numerical variables from text where possible
+	data['numerical'] = convert_to_numeric(data['text'])
 	
-	# Filter to those which begin with the same first four characters
-	dat_agg = dat_agg[dat_agg['text_agg'].apply(lambda x: x.startswith(keyphrase[:4]))]
-	
-	print(dat_agg.iloc[0:2][['text', 'text_agg']])
+	# Find the balance sheet pages
+	csv_numbers = find_balance_sheet_pages(data)
 	
 	results = pd.DataFrame()
 	
-	# Take first result, hunt down all numbers aligned with it
-	for index, row in dat_agg.iloc[0:2].iterrows():
-	
-		selection = extract_parallel_numbers(dat, row)		   
-
-		# Record some metadata about the match found					  
-		selection['text_matched'] = row['text_agg']
-		selection['text_original'] = row['text']
+	# Iterate through balance sheet pages, retrieve everything
+	for csv_number in csv_numbers:
+		page_df = data[data['csv_num'] == csv_number]
 		
-		results = results.append(selection)
-	
-	return(results)
-	
-
-def find_statistics(dat, keystring):
-	"""
-	Try to get a statistic based upon a key phrase or word.
-	First tries to get it by searching the exact keyword/s,
-	then if that returns nothing it tries a more complex method
-	over multiple (2) lines.
-	"""
-	
-	try:
-		stats = extract_stat(dat, keystring)
-		if len(stats) > 0:
-			return(stats)
-	
-	except:
+		# Determine where the lines are
+		detected_lines = detect_lines(page_df)
 		
-		try:
-			stats = extract_stat_complex(dat, keystring)
-			if len(stats) > 0:
-				return(stats)
-		
-		except Exception as e:
-			print("Couldn't find a stat: " + keystring + ", exception: " + str(e))
+		# Get all detectable balance sheet stats
+		results = results.append(extract_lines(page_df, detected_lines))
 	
-	return(0)
-
-
-
-
-
-
+	return( results )
+	
